0a329655b808397c93a0d6dcae39bf1baa8b5e7b
Gavin Sharkey <gavinsharkey2016@gmail.com>
2020-03-30 17:17:56 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,16 +1,16 @@
 INSERT INTO bears (name, age, gender, color, temperament, alive)
-VALUES ('Mr. Chocolate', 7, 'male', 'brown', 'evil', true);
+VALUES ('Mr. Chocolate', 7, 'male', 'brown', 'evil', 1);
 INSERT INTO bears (name, age, gender, color, temperament, alive)
-VALUES ('Rowdy', 7, 'male', 'brown', 'evil', true);
+VALUES ('Rowdy', 7, 'male', 'brown', 'evil', 1);
 INSERT INTO bears (name, age, gender, color, temperament, alive)
-VALUES ('Tabitha', 7, 'male', 'brown', 'evil', true);
+VALUES ('Tabitha', 7, 'male', 'brown', 'evil', 1);
 INSERT INTO bears (name, age, gender, color, temperament, alive)
-VALUES ('Sergeant Brown', 7, 'male', 'brown', 'evil', true);
+VALUES ('Sergeant Brown', 7, 'male', 'brown', 'evil', 1);
 INSERT INTO bears (name, age, gender, color, temperament, alive)
-VALUES ('Melissa', 7, 'male', 'brown', 'evil', true);
+VALUES ('Melissa', 7, 'male', 'brown', 'evil', 1);
 INSERT INTO bears (name, age, gender, color, temperament, alive)
-VALUES ('Grinch', 7, 'male', 'brown', 'evil', true);
+VALUES ('Grinch', 7, 'male', 'brown', 'evil', 1);
 INSERT INTO bears (name, age, gender, color, temperament, alive)
-VALUES ('Wendy', 7, 'male', 'brown', 'evil', true);
+VALUES ('Wendy', 7, 'male', 'brown', 'evil', 1);
 INSERT INTO bears (name, age, gender, color, temperament, alive)
-VALUES (NULL, 7, 'male', 'brown', 'evil', true);
+VALUES (NULL, 7, 'male', 'brown', 'evil', 1);
